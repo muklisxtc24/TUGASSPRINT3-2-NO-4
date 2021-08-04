b2a857dfd79d566f5c5573402c08f7ec913751d1
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-08-05 04:59:12 +0700

    Add files via upload

---
+++ TUGASSP3-2NO4.js
@@ -0,0 +1,6 @@
+var kodeUnixRandom = [12,78,42,40,36,2,8,96,66,20,91,85,41,69,38,77,97,60,15,57];
+
+const NilaiHasil = kodeUnixRandom.reduce((accumulator,currentValue) => (accumulator + currentValue) / kodeUnixRandom.length);
+
+console.log(NilaiHasil);var kodeUnixRandom = [12,78,42,40,36,2,8,96,66,20,91,85,41,69,38,77,97,60,15,57];
+
